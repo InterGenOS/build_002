41d0291328a4b732e332ffc8e0cd24a2f0e1fffc
InterGenOS <chris@intergenstudios.com>
2015-04-07 05:18:18 -0500

    corrected log directive

--- build_sys.sh
+++ build_sys.sh
@@ -483,7 +483,7 @@ tar xf zlib-1.2.8.tar.xz &&
 cd zlib-1.2.8
 ./configure --prefix=/usr &&
 make &&
-make check > /zlib-mkck-log_$(date +"%m-%d-%Y_%T") &&
+make check >> /zlib-mkck-log_$(date +"%m-%d-%Y_%T") &&
 make install &&
 mv -v /usr/lib/libz.so.* /lib
 ln -sfv ../../lib/$(readlink /usr/lib/libz.so) /usr/lib/libz.so
